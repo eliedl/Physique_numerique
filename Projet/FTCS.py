45c3986a12aa7a709cf87cd68444f4be1594a60e
moumoux1 <eliedl1596@gmail.com>
2018-04-23 09:01:49 -0400

    Ajout solution equation de diffusion avec FTCS

--- Projet/FTCS.py
+++ Projet/FTCS.py
@@ -64,14 +64,76 @@ def propagation(L, N, tmax = 0.1, Nt = 1000, fps = 50, plot=False):
 
     return res
 
+
+def diffusion(N, h, L=0.01, D=4.25e-6):
+
+    a = L/N
+    epsilon = h/1000
+
+    x = np.linspace(0, L, N+1)
+
+    Tlo = 0
+    Tmid = 20
+    Thi = 50
+
+    t1 = 0.01
+    t2 = 0.1
+    t3 = 0.4
+    t4 = 1
+    t5 = 10
+
+    tend = t5 + epsilon
+
+    T = np.zeros(N+1)
+    T[0] = Thi
+    T[N] = Tlo
+    T[1:N] = Tmid
+
+    Tp = np.zeros(N + 1)
+    Tp[0] = Thi
+    Tp[N] = Tlo
+
+    t = 0
+    c = h*D/(a**2)
+
+    while t < tend:
+
+        Tp[1:N] = T[1:N] + c*(T[0:N-1] + T[2:N+1] -2*T[1:N])
+
+        T, Tp = Tp, T
+        t+= h
+
+        if abs(t-t1)<epsilon:
+            plt.plot(x, T, c='C2', alpha=0.2, label='0.01')
+        if abs(t - t2) < epsilon:
+            plt.plot(x, T, c='C2', alpha=0.4, label='0.1')
+        if abs(t - t3) < epsilon:
+            plt.plot(x, T, c='C2', alpha=0.6, label='0.4')
+        if abs(t - t4) < epsilon:
+            plt.plot(x, T, c='C2', alpha=0.8, label='1')
+        if abs(t - t5) < epsilon:
+            plt.plot(x, T, c='C2', alpha=1, label='10')
+
+    plt.legend()
+    plt.xlabel('$x$ [m]')
+    plt.ylabel(r'$T$ [$^\circ$C]')
+    plt.show()
+
+
+
+
+
 if __name__ == '__main__':
 
-    N = 1000
-    x = np.linspace(0, 1, N)
-    phi = np.zeros(N+1)
-    psi = speed(x)
-    tmax = 0.090
-    x = propagation(1, N, tmax = tmax, Nt = 5e6, fps = 500//tmax)
-    np.save("carlos",x)
+    #N = 1000
+    #x = np.linspace(0, 1, N)
+    #phi = np.zeros(N+1)
+    #psi = speed(x)
+    #tmax = 0.090
+    #x = propagation(1, N, tmax = tmax, Nt = 5e6, fps = 500//tmax)
+    #np.save("carlos",x)
+
+    plt.style.use('seaborn-talk')
+    diffusion(100, 1e-4)
 
 
